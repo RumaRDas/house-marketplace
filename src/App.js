f7bce7875ade8d7a8a0d764333ca55d13081039b
Ruma Das <rumaadl16@gmail.com>
2022-04-23 23:53:38 +0930

    adding navbar with link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -18,8 +19,8 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
-      {/* Navbar*/}
     </>
   )
 }
